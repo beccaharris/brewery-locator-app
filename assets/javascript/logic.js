7b434a26692c47a167e5f140c16ad1fdaaaf37c9
danejackins <dane.jackins@gmail.com>
2018-03-19 20:59:29 -0600

    some styling changes

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   console.log( 'ready!' );
   var beerFacts = [
     {fact: 'Beer is made with hops!'},
-    {fact: 'You mamma has three titties: one for milk, one for water, and the third one says "OUT OF ORDER"'}
+    {fact: 'Your mamma has three titties: one for beer, one for water, and the third one says "OUT OF ORDER"'}
   ]
   console.log(beerFacts);
   // Append beerFacets to .beer-facts
