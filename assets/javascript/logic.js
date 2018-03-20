38173e644a9a24854510b403d186c78e97866ffd
Becca Harris <beccalynharris@gmail.com>
2018-03-19 21:28:05 -0600

    get beer api working

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -8,19 +8,21 @@ $(document).ready(function() {
   // Append beerFacets to .beer-facts
 
   // Cycle through array on 30 second timer
+    var userInput;
+    $('#search-button').on('click', function(event) {
+      userInput = $('#zip-code').val().trim();
+      console.log(userInput)
+      var queryUrl = 'https://thingproxy.freeboard.io/fetch/http://beermapping.com/webservice/loccity/E3b1372b6db3c5e549e795a11ed77331/' + userInput + '&s=json'
+      $.ajax({
+        url: queryUrl,
+        method: 'GET'
+      }).done(function(beer) {
+        var beerList = beer.data;
+        beer.forEach(function(brewery) {
+          console.log(brewery.name)
+        })
+      })
+    });
   });
 
-  var userInput;
-  $('#search-button').on('click', function(event) {
-    userInput = $('#zip-code').val().trim();
-    console.log(userInput)
-    var queryUrl = 'https://cors-anywhere.herokuapp.com/http://beermapping.com/webservice/loccity/E3b1372b6db3c5e549e795a11ed77331/' + userInput
-  $.ajax({
-    url: queryUrl,
-    method: 'GET'
-  }).done(function(beer) {
-    var beerList = beer.data;
-    console.log(queryUrl)
-    console.log(beer)
-  })
-});
+
