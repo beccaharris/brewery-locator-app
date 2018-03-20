019a0ab0d58886e0ef7ee1cf18a9d415b05ebf72
danejackins <dane.jackins@gmail.com>
2018-03-19 19:52:29 -0600

    starting on logic

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,3 +1,12 @@
 $(document).ready(function() {
-  console.log( "ready!" );
+  console.log( 'ready!' );
+  var beerFacts = [
+    {fact: 'Beer is made with hops!'},
+    {fact: 'You mamma has three titties: one for milk, one for water, and the third one says "OUT OF ORDER"'}
+  ]
+
+  console.log(beerFacts);
+  // Append beerFacets to .beer-facts
+
+  // Cycle through array on 30 second timer
 });
