6000c44da89affb660ba06cf8cb3e818bdaa084f
Becca Harris <beccalynharris@gmail.com>
2018-03-22 23:52:18 -0600

    omg datatables are working. what...a win

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -10,81 +10,80 @@ $(document).ready(function () {
   // Cycle through array on 30 second timer
 
 
-  var array1 = []
-  var array2 = []
-  var array3 = []
-
+// =========================================================== //
+// ====================== BEERMAPPING ======================== //
+// =========================================================== //
   var userInput;
-  $('#search-button').on('click', function (event) {
+  
+  $('#search-button').on('click', function(event) {
     userInput = $('#city-search').val().trim();
     $('#beer-table > tbody').empty();
-    $('#pagination').empty();
-    console.log(userInput)
     var queryUrl = 'https://thingproxy.freeboard.io/fetch/http://beermapping.com/webservice/loccity/E3b1372b6db3c5e549e795a11ed77331/' + userInput + '&s=json'
     $.ajax({
       url: queryUrl,
-      method: 'GET'
-    }).done(function (beer) {
-      var beerList = beer;
-      console.log(beer.length);
-      console.log(beerList);
-
-      var arrayOfArrays = [];
-
-      var numberOf10ElementArrays = beerList.length / 10; // How many pages of 10 items you can get from the response
-
-      for (let i = 0; i < numberOf10ElementArrays; i++) {
-        let array = [];
-        console.log('-----------------------------')
-        for (let j = 0; j < 10; j++) {
-          console.log(beerList[j]);
-          if (beerList[j]) {
-            array.push(beerList[j]);
-            beerList.splice(j, 1);
-          }
+      method: 'GET',
+      success: function(obj, textstatus) {
+        if ($.fn.DataTable.isDataTable("#beer-table")) {
+          $('#beer-table').DataTable().clear().destroy();
         }
-        arrayOfArrays.push(array);
+        $('#beer-table').DataTable({
+          data: obj,
+          columns: [
+            {data: 'id'},
+            {data: 'name'},
+            {data: 'status'},
+            {data: 'reviewlink'},
+            {data: 'proxylink'},
+            {data: 'blogmap'},
+            {data: 'street'},
+            {data: 'city'},
+            {data: 'state'},
+            {data: 'zip'},
+            {data: 'country'},
+            {data: 'phone'},
+            {data: 'url'},
+            {data: 'overall'},
+            {data: 'imagecount'}
+          ],
+          "columnDefs" : [
+            {
+              "targets": [0],
+              "visible": false
+            },{
+              "targets": [2],
+              "visible": false
+            },{
+              "targets": [3],
+              "visible": false
+            },{
+              "targets": [4],
+              "visible": false
+            },{
+              "targets": [5],
+              "visible": false
+            },{
+              "targets": [10],
+              "visible": false
+            },{
+              "targets": [11],
+              "visible": false
+            },{
+              "targets": [12],
+              "visible": false
+            },{
+              "targets": [13],
+              "visible": false
+            },{
+              "targets": [14],
+              "visible": false
+            }
+          ]
+        })
       }
-
-      console.log(arrayOfArrays);
-
-      arrayOfArrays.forEach((array, index) => {
-        var numIndicator = $(`<button class="pagination-btn" data-index=${index}>${index}</button>`);
-       // want to empty the page divs // 
-        $('#pagination').append(numIndicator);
-      });
-
-      $('#pagination').on('click', '.pagination-btn', function () {
-        var whichButton = ($(this).data('index'));
-        console.log(arrayOfArrays[whichButton]);
-        $('#beer-table > tbody').empty();
-        arrayOfArrays[whichButton].forEach(function (brewery) {
-          $('#beer-table > tbody').append(
-            '<tr>' +
-            '<td><a class="map-me">Map Me!' +
-            '<td>' + brewery.name + '</td>' +
-            '<td>' + brewery.street + '</td>' +
-            '<td>' + brewery.city + '</td>' +
-            '<td>' + brewery.state + '</td>' +
-            '<tr>'
-          );
-        });
-      });
-
-
-      arrayOfArrays[0].forEach(brewery => {
-        $('#beer-table > tbody').append(
-          '<tr>' +
-          '<td><a class="map-me">Map Me!' +
-          '<td>' + brewery.name + '</td>' +
-          '<td>' + brewery.street + '</td>' +
-          '<td>' + brewery.city + '</td>' +
-          '<td>' + brewery.state + '</td>' +
-          '<tr>'
-        );
-      })
-    })
-  });
+    }) 
+  })
 });
+
+
 
 
