f23e6491421282005d16100e1bc35c9819680124
Becca Harris <beccalynharris@gmail.com>
2018-03-26 17:38:17 -0600

    start foundation work

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,9 +1,12 @@
 $(document).ready(function() {
+  // =========================================================== //
+  // ====================== BEER FACTS  ======================== //
+  // =========================================================== //
   var beerFacts = [
-    {fact: 'At any given time, 0.7% of the world is drunk. So 50 million people are drunk right now.'},
-    {fact: "The worlds longest hangover lasted 4 weeks after a Scotsman consumed 60 pints of beer."},
-    {fact: 'Steven Petrosino of New Cumberland, Pennsylvania downed 1 liter of beer or 33 ounces in a chilly 1.3 seconds in 1977 which made him a World Beer Chugging Champion according to the Guinness Book of World Records.'},
-    {fact: 'Amsterdam pays alcoholics in beer. For cleaning the city streets, local alcoholics get 10 Euros, half a packet of rolling tobacco and 5 beers as payment by a government-funded organization.'},
+    {fact: 'At any given time, 0.7% of the world is drunk. So, 50 million people are drunk right now.'},
+    {fact: "The world's longest hangover lasted 4 weeks after a Scotsman consumed 60 pints of beer."},
+    {fact: 'Steven Petrosino of New Cumberland, Pennsylvania downed 1 liter of beer (or 33 ounces) in a chilly 1.3 seconds in 1977, which made him a World Beer Chugging Champion according to the Guinness Book of World Records.'},
+    {fact: 'Amsterdam pays alcoholics in beer. For cleaning the city streets, local alcoholics get 10 Euros, half a packet of rolling tobacco, and 5 beers as payment by a government-funded organization.'},
     {fact: 'In 1814, almost 400,000 gallons of beer flooded several streets in London after a huge vat ruptured in the parish of St. Giles.'},
     {fact: "Beer commercials in the US aren't really allowed to show people actually drinking the beer. It's a US law that people cannot actually be shown consuming an alcoholic beverage on television."},
     {fact: 'The world´s strongest beer is Brewmeister´s "Snake Venom“. While regular beer usually have about 5% ABV, this Scottish killer has a stomach-burning 67,5% ABV.'},
@@ -42,9 +45,13 @@ $(document).ready(function() {
   factRotation(Math.floor(Math.random() * beerFacts.length));
 
 // =========================================================== //
-// ====================== BEERMAPPING ======================== //
+// ====================== BEERMAPPING API CALL================ //
 // =========================================================== //
   var userInput;
+
+  if ($('#city-search') == '') {
+    $('#search-button').attr('disabled')
+  }
   
   $('#search-button').on('click', function(event) {
     userInput = $('#city-search').val().trim();
@@ -133,12 +140,12 @@ $(document).ready(function() {
   $("#submit").on("click", function (event) {
     event.preventDefault();
     var name = $("#name").val().trim();
-    console.log(name);
     var age = $("#age").val().trim();
-    console.log(age)
     var comment = $("#comment").val().trim();
-    console.log(comment)
 
+    $("#name").on("invalid.zf.abide", function(ev,el) {
+      alert("Input field foo is invalid");
+    });
     var newComment = {
       newName: name,
       newAge: age,
@@ -175,7 +182,6 @@ $(document).ready(function() {
 
     });
   });
-
 
 });
 
