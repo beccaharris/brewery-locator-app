17aee3156d01cf17f37067bd87e4bdffc78d87dc
danejackins <dane.jackins@gmail.com>
2018-03-28 20:59:16 -0600

    shut up Becca

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -12,7 +12,7 @@ $(document).ready(function () {
     { fact: 'Steven Petrosino of New Cumberland, Pennsylvania downed 1 liter of beer (or 33 ounces) in a chilly 1.3 seconds in 1977, which made him a World Beer Chugging Champion according to the Guinness Book of World Records.' },
     { fact: 'Amsterdam pays alcoholics in beer. For cleaning the city streets, local alcoholics get 10 Euros, half a packet of rolling tobacco, and 5 beers as payment by a government-funded organization.' },
     { fact: 'In 1814, almost 400,000 gallons of beer flooded several streets in London after a huge vat ruptured in the parish of St. Giles.' },
-    { fact: "Beer commercials in the US aren't really allowed to show people actually drinking the beer. It's a US law that people cannot actually be shown consuming an alcoholic beverage on television." },
+    { fact: "Beer commercials in the U.S. aren't really allowed to show people actually drinking the beer. It's a US law that people cannot actually be shown consuming an alcoholic beverage on television." },
     { fact: 'The world´s strongest beer is Brewmeister´s "Snake Venom". While regular beer usually have about 5% ABV, this Scottish killer has a stomach-burning 67.5% ABV.' },
     { fact: 'The most beer-drinking country in the world is the Czech Republic. With an incredible per capita beer consumption of almost 40 gallons a year, the Czechs are way out in front in the beer drinking world league table.' },
     { fact: 'Old Vikings believed that in their heaven called Valhalla, there is a giant goat whose udders provided unlimited supply of beer.' },
@@ -42,7 +42,7 @@ $(document).ready(function () {
           i = 0
         }
         factRotation(i + 1);
-      }, 25000);
+      }, 45000);
     };
   };
 
