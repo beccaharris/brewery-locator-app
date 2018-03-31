688898fa18c3ff5b80124e14b7598d8714d0c9c0
Becca Harris <beccalynharris@gmail.com>
2018-03-31 02:49:09 -0600

    finish landing page

--- assets/javascript/landingPage.js
+++ assets/javascript/landingPage.js
@@ -1,33 +1,24 @@
-var beerMemes = ['assets/images/memes/meme1.png', 'assets/images/memes/meme2.jpg', 'assets/images/memes/meme3.jpeg', 'assets/images/memes/meme4.jpeg', 'assets/images/memes/meme5.jpg', 'assets/images/memes/meme6.jpg', 'assets/images/memes/meme7.jpg', 'assets/images/memes/meme8.jpg']
 var breweries = [
   { 
-    //breweryName: '<a target="_blank" href="www.alpinedogbrewery.com">Alpine Dog Brewery</a>',
+    breweryUrl: 'http://www.alpinedogbrewery.com',
     breweryName: 'Alpine Dog Brewery',
+    breweryCityState: 'Denver, CO',
     breweryImg: '<img class="brewery-image" src="assets/images/breweries/alpinedog.png">',
   }, {
-    //breweryName: '<a target="_blank" href="www.cerebralbrewing.com">Cerebral Brewing</a>',
-    breweryName: '<a target="_blank" href="www.cerebralbrewing.com">Cerebral Brewing</a>',
+    breweryUrl: 'http://www.cerebralbrewing.com',
+    breweryName: 'Cerebral Brewing',
+    breweryCityState: 'Denver, CO',
     breweryImg: '<img class="brewery-image" src="assets/images/breweries/cerebral.jpg">',
   }, {
-    //breweryName: '<a target="_blank" href="www.drydockbrewing.com">Dry Dock Brewing Co.</a>',
-    breweryName: '<a target="_blank" href="www.drydockbrewing.com">Dry Dock Brewing Co.</a>',
-    breweryImg: '<img class="brewery-image" src="assets/images/breweries/drydock.jpg">',
-  }, {
-    //breweryName: '<a target="_blank" href="www.postbrewing.com">The Post Brewing Co.</a>',
-    breweryName: '<a target="_blank" href="www.postbrewing.com">The Post Brewing Co.</a>',
+    breweryUrl: 'http://www.postbrewing.com',
+    breweryName: 'The Post Brewing Co.',
+    breweryCityState: 'Lafayette, CO',
     breweryImg: '<img class="brewery-image" src="assets/images/breweries/post.jpeg">',
   }, {
-    //breweryName: '<a target="_blank" href="www.ratiobeerworks.com/">Ratio Beerworks</a>',
+    breweryUrl: 'http://www.ratiobeerworks.com',
     breweryName: 'Ratio Beerworks',
+    breweryCityState: 'Denver, CO',
     breweryImg: '<img class="brewery-image" src="assets/images/breweries/ratiobeerworks.png">',
-  }, {
-    //breweryName: '<a target="_blank" href="www.station26brewing.co">Station 26 Brewing Co.</a>',
-    breweryName: 'Station 26 Brewing Co.',
-    breweryImg: '<img class="brewery-image" src="assets/images/breweries/station26.png">',
-  }, {
-    //breweryName: '<a target="_blank" href="www.ursulabrewery.com">Ursula Brewery</a>',
-    breweryName: 'Ursula Brewery',
-    breweryImg: '<img class="brewery-image" src="assets/images/breweries/ursulabrew.jpg">',
   }
 ]
 
@@ -35,12 +26,18 @@ $(document).ready(function () {
 
   for (var i = 0; i < breweries.length; i++) {
     var eachBreweryImage = breweries[i].breweryImg
+    var eachBreweryName = breweries[i].breweryName
+    var eachBreweryUrl = breweries[i].breweryUrl
+    var newLink = $('<a>')
+    newLink.attr('href', breweries[i].breweryUrl)
+    
+    var eachBreweryCitySt = breweries[i].breweryCityState
+
     newListItemImg = $('<li>')
     
-    $(newListItemImg).append(eachBreweryImage);
+    $(newListItemImg).append(eachBreweryImage + '<br><a target="_blank" href="' + eachBreweryUrl + '">'  + eachBreweryName + '</a><br><p>' + eachBreweryCitySt);
     //$(newColumnImg).append(breweryNames[i])
     $('#brewery-picks').append(newListItemImg)
 
   }
-  $('#memes').append('<img id="beer-meme" src="'+ beerMemes[(Math.floor(Math.random() * beerMemes.length))] + '">')
 }) 
